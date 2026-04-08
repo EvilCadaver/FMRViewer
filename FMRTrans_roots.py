c97323fae6968d77f2dad3321663cf46694bdc24
Denys Musiienko <68948002+EvilCadaver@users.noreply.github.com>
2026-04-08 15:40:24 +0200

    fixed missing coefficient in c

--- FMRTrans_roots.py
+++ FMRTrans_roots.py
@@ -7,25 +7,25 @@ def correct_sintheta(vals, eps=1e-5):
 
 Hk = 5.0 #kOe
 Ms = 8 #kGauss
-phi = 15 #deg
+phi = 45 #deg
 
 phi = np.radians(phi)
 A = Hk*np.sin(2*phi)
 B = 4*np.pi*Ms + Hk*np.cos(2*phi)
 C = Hk/2*np.sin(2*phi)
 
-H = 5 #kOe
+H = 10 #kOe
 
-# (A^2+B^2)*x^4 + 2*A*H*x^3 - (2*A*C+B^2)*x^2 - 2*H*C*x + C^2 = 0
-# (A^2+B^2)*x^4 + 2*A*H*x^3 - (A^2+B^2)*x^2 - H*A*x + A^2/4 = 0
+# (A^2 + B^2)*x^4 + 2*A*H*x^3 + (H^2 - 2*A*C - B^2)*x^2 - 2*H*C*x + C^2 = 0
+# (A^2 + B^2)*x^4 + 2*A*H*x^3 + (H^2 - A^2 - B^2)*x^2 - H*A*x + A^2/4 = 0
 
 a = (A**2+B**2)
 b = 2*A*H
-c = -(2*A*C+B**2)
+c = H**2-(A**2+B**2)
 d = -H*A
 e = A**2/4
 
-coeffs = [a, b, -a, d, e]
+coeffs = [a, b, c, d, e]
 print("Roots for sin(theta):", roots := np.roots(coeffs))
 
 print("Checking for H:")
