5143cfa9de36030d898cd8ea037fdaedf1d37e18
Denys Musiienko <68948002+EvilCadaver@users.noreply.github.com>
2026-04-23 16:45:34 +0200

    some sanity recuperated

--- FMRTrans_roots.py
+++ FMRTrans_roots.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-def correct_sintheta(vals, eps=1e-5):
-    normalised = [0.0 if abs(x) <= eps else x for x in vals]
-    candidates = [x for x in normalised if x>=0]
-    return min(candidates) if candidates else None
+def correct_sintheta(vals):
+    vals = [x for x in vals if x>0]
+    return min(vals) if vals else None
 
 Hk = 4.0 #kOe
 Ms = 6 #kGauss
-phi = 20 #deg
-Ho = 10 #kOe
+phi = 50 #deg
+Ho = 1 #kOe
 
 def find_thetas(H = Ho, Hk = Hk, Ms = Ms, phi = phi):
     phi = np.radians(phi)
@@ -29,18 +28,16 @@ def find_thetas(H = Ho, Hk = Hk, Ms = Ms, phi = phi):
     theta1 = [np.asin(x) for x in roots]
     theta2 = [np.pi - np.asin(x) for x in roots]
     thetas = theta1 + theta2
-    eps = 1e-10
-    true_thetas = [theta for theta in thetas if abs(A*np.sin(theta)*np.cos(theta)-B/2+B*np.sin(theta)**2+H*np.sin(theta))<eps]
+    eps = 1e-7
+    true_thetas = [theta for theta in thetas if (abs(A*np.sin(theta)*np.cos(theta)-B/2+B*np.sin(theta)**2+H*np.sin(theta))<eps) and (abs(theta) % np.pi>eps)]
     print('Useable thetas:', np.rad2deg(true_thetas))
     print('Equation check:', [(A*np.sin(theta)*np.cos(theta)-B/2+B*np.sin(theta)**2+H*np.sin(theta)) for theta in true_thetas])
-    print('H with replacements check:', [B/np.sin(theta)-2*B*np.sin(theta)-A*np.cos(theta) for theta in true_thetas])
-    print('H original check:', [Hk/2*np.sin(2*(phi-theta))/np.sin(theta) - 4*np.pi*Ms*np.cos(theta) for theta in theta2])
+    print('H with replacements check:', [B/np.sin(theta)/2-B*np.sin(theta)-A*np.cos(theta) for theta in true_thetas])
+    print('H original check:', [Hk/2*np.sin(2*(phi-theta))/np.sin(theta) - 4*np.pi*Ms*np.cos(theta) for theta in true_thetas])
     return true_thetas
 
 print("Checking for H:")
-for sintheta in (roots := find_thetas()):
-    if sintheta**2<=1:
-        theta = np.asin(sintheta)
-        print(f"Theta = {np.rad2deg(theta):.4f}")
+for theta in (roots := find_thetas()):
+    print(f"Theta = {np.rad2deg(theta):.4f}")
 
-print("Chosen theta =", f"{np.rad2deg(theta := np.asin(correct_sintheta(roots))):.5f}")
+print("Chosen theta =", f"{np.rad2deg(correct_sintheta(roots)):.5f}")
