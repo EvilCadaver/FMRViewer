1c4768b8d3e386dd393f4bb007d7846a57777e80
Denys Musiienko <68948002+EvilCadaver@users.noreply.github.com>
2026-04-23 16:20:19 +0200

    sanity checks failed

--- FMRTrans_roots.py
+++ FMRTrans_roots.py
@@ -12,22 +12,30 @@ Ho = 10 #kOe
 
 def find_thetas(H = Ho, Hk = Hk, Ms = Ms, phi = phi):
     phi = np.radians(phi)
-    A = 4*np.pi*Ms-Hk*(2*np.sin(phi)**2-1)
-    B = Hk*np.sin(2*phi)/2
-    # (A**2+B**2)*x**4 + 2*B*H*x**3 + (H**2 - 4*B**2 - A**2)*x**2 - 4*B*H*x + 4*B**2 = 0, x = sin(theta)
+    A = 4*np.pi*Ms-Hk*(np.sin(phi)**2-np.cos(phi)**2)
+    B = Hk*np.sin(2*phi)
+    # (A**2+B**2)*x**4 + 2*B*H*x**3 + (H**2 - A**2 - B**2)*x**2 - B*H*x + B**2/4 = 0, x = sin(theta)
     a = (A**2+B**2)
     b = 2*B*H
-    c = (H**2 - 4*B**2 - A**2)
-    d = - 4*B*H
-    e = 4*B**2
+    c = (H**2 - B**2 - A**2)
+    d = - B*H
+    e = B**2/4
     
     coeffs = [e, d, c, b, a]
     p = np.polynomial.Polynomial(coeffs)
     roots = p.roots()
 
     print('Roots: ', roots)
-    print('H check: ', [(H*x + 2*np.pi*Ms*np.sin(2*np.asin(x))-Hk*np.sin(2*(phi-np.asin(x)))) for x in roots])
-    return roots
+    theta1 = [np.asin(x) for x in roots]
+    theta2 = [np.pi - np.asin(x) for x in roots]
+    thetas = theta1 + theta2
+    eps = 1e-10
+    true_thetas = [theta for theta in thetas if abs(A*np.sin(theta)*np.cos(theta)-B/2+B*np.sin(theta)**2+H*np.sin(theta))<eps]
+    print('Useable thetas:', np.rad2deg(true_thetas))
+    print('Equation check:', [(A*np.sin(theta)*np.cos(theta)-B/2+B*np.sin(theta)**2+H*np.sin(theta)) for theta in true_thetas])
+    print('H with replacements check:', [B/np.sin(theta)-2*B*np.sin(theta)-A*np.cos(theta) for theta in true_thetas])
+    print('H original check:', [Hk/2*np.sin(2*(phi-theta))/np.sin(theta) - 4*np.pi*Ms*np.cos(theta) for theta in theta2])
+    return true_thetas
 
 print("Checking for H:")
 for sintheta in (roots := find_thetas()):
