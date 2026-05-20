Radians fix number 2

FMRuWire_Model_graphing.py
@@ -50,8 +50,8 @@ def find_thetas(H = H0, Hk = H_K, Ms = M_S, phi = PHI):
     return true_thetas
 
 def mu_eff(eta = 0, H = H0, Hk = H_K, Ms = M_S, phi = PHI, omg = omg, alpha = ALPHA):
-    phi = np.radians(phi)
     theta = correct_theta(find_thetas(H= H, Hk= Hk, Ms= Ms, phi= phi))
+    phi = np.radians(phi)
     Heff = H*np.cos(theta) - 2*np.pi*Ms*np.sin(theta)**2 + Hk*np.cos(phi-theta)**2
     A = 4*np.pi*Ms*np.cos(theta)*np.sin(eta)*np.cos(eta)
     B = Heff + 4*np.pi*Ms*np.sin(eta)**2

FMRuWire_Model_pptx.py
@@ -12,7 +12,7 @@ from scipy.optimize import minimize_scalar
 H0 = float(5) #kOe
 H_K = float(0.5) #kOe
 M_S = float(0.65) #kGauss
-PHI = float(45) #deg
+PHI = float(45.0) #deg
 ALPHA = 1e-3 #Gilbert damping
 GYROMAG_FACTOR = float(2.0) 
 FREQ = float(36) #GHz
@@ -20,7 +20,6 @@ GAMMA = 1.399611 #GHz/kOe
 omg = (FREQ/(GAMMA*GYROMAG_FACTOR)) #omega/gamma 
 
 def find_theta(H = H0, Hk = H_K, Ms = M_S, phi = PHI):
-    phi = np.radians(phi)
     def Energy(theta):
         return -Ms*H*np.cos(theta) + 2*np.pi*Ms**2*np.sin(theta)**2 + Hk*Ms/2*np.sin(phi - theta)**2
     result = minimize_scalar(Energy, bounds=(np.deg2rad(-1),np.deg2rad(5)), method="bounded")